61c3a6ad84fa6e0771aec197a8fa9c4f419bfd9f
mikoto2000 <mikoto2000@gmail.com>
2026-03-23 07:17:45 +0900

    Spring Initializr.
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=4.0.4&packaging=jar&configurationFileFormat=yaml&jvmVersion=25&groupId=dev.mikoto2000&artifactId=rei&packageName=dev.mikoto2000.rei&dependencies=native,devtools,lombok,spring-ai-ollama,spring-ai-chat-memory-repository-jdbc

---
+++ src/main/java/dev/mikoto2000/rei/ReiApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.rei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mikoto2000/rei/ReiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.rei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
