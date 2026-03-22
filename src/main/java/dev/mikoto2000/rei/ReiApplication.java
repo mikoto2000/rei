f59fe1f33edf0f0bb19133671ff5230a0346614b
mikoto2000 <mikoto2000@gmail.com>
2026-03-22 22:55:17 +0000

    Ollama サーバーとの疎通確認。

--- src/main/java/dev/mikoto2000/rei/ReiApplication.java
+++ src/main/java/dev/mikoto2000/rei/ReiApplication.java
@@ -1,13 +1,33 @@
 package dev.mikoto2000.rei;
 
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @SpringBootApplication
 public class ReiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReiApplication.class, args);
-	}
+  private final ChatModel chatModel;
+
+  public  static void main(String[] args) {
+    var context = SpringApplication.run(ReiApplication.class, args);
+    var app = context.getBean(ReiApplication.class);
+    app.run(args);
+    context.close();
+  }
+
+  private void run(String[] args) {
+    ChatResponse chatResponse = chatModel.call(new Prompt("こんにちは！"));
+
+    String thinking = chatResponse.getResult().getMetadata().get("thinking");
+    IO.println(thinking);
+    String answer = chatResponse.getResult().getOutput().getText();
+    IO.println(answer);
+  }
 
 }
