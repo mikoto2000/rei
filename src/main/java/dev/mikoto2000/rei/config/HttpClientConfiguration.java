aab214b936487a64aef5d14bf30b3552164b03e9
mikoto2000 <mikoto2000@gmail.com>
2026-07-28 23:10:24 +0900

    Add mDNS support.

--- src/main/java/dev/mikoto2000/rei/config/HttpClientConfiguration.java
+++ src/main/java/dev/mikoto2000/rei/config/HttpClientConfiguration.java
@@ -2,9 +2,11 @@ package dev.mikoto2000.rei.config;
 
 import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.boot.restclient.RestClientCustomizer;
+import org.springframework.boot.webclient.WebClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ReactorClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import reactor.netty.http.client.HttpClient;
 
 @Configuration
@@ -12,13 +14,20 @@ public class HttpClientConfiguration {
 
     @Bean
     RestClientCustomizer mdnsRestClientCustomizer() {
-        HttpClient httpClient = HttpClient.create()
-            .resolver(DefaultAddressResolverGroup.INSTANCE);
-
         ReactorClientHttpRequestFactory requestFactory =
-            new ReactorClientHttpRequestFactory(httpClient);
+            new ReactorClientHttpRequestFactory(mdnsHttpClient());
 
         return builder -> builder.requestFactory(requestFactory);
     }
-}
 
+    @Bean
+    WebClientCustomizer mdnsWebClientCustomizer() {
+        ReactorClientHttpConnector connector = new ReactorClientHttpConnector(mdnsHttpClient());
+        return builder -> builder.clientConnector(connector);
+    }
+
+    private HttpClient mdnsHttpClient() {
+        return HttpClient.create()
+            .resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+}
